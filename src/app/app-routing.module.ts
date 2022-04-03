289bbce9504f699fabd7c303e55038bb3a6f8a1a
DmitriiBiblev <biblev1608@gmail.com>
2022-04-03 03:16:50 +0300

    created pages, layout and develop component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevelopComponent } from './shared/modules/develop/develop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/news',
+    pathMatch: 'full',
+  },
+  {
+    path: 'news',
+    // loadChildren: () => import('./news/news.module').then(m => m.NewsModule)
+    component: DevelopComponent,
+  },
+  {
+    path: 'craft',
+    // loadChildren: () => import('./craft/craft.module').then(m => m.CraftModule)
+    component: DevelopComponent,
+  },
+  {
+    path: 'recraft',
+    // loadChildren: () => import('./recraft/recraft.module').then(m => m.RecraftModule)
+    component: DevelopComponent,
+  },
+  {
+    path: 'donations',
+    // loadChildren: () => import('./donations/donations.module').then(m => m.DonationsModule)
+    component: DevelopComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/shared/modules/layout/data/nav-items.data.ts
@@ -0,0 +1,20 @@
+import { INavItem } from '../interfaces/nav-item.interface';
+
+export const navItems: INavItem[] = [
+  {
+    link: 'news',
+    text: 'Новости',
+  },
+  {
+    link: 'craft',
+    text: 'Крафт',
+  },
+  {
+    link: 'recraft',
+    text: 'Перекрафт',
+  },
+  {
+    link: 'donations',
+    text: 'Поддержать проект',
+  },
+];
